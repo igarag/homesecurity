ba18b2c24e64f65aef6862ab51b58b7037a79bd0
igarag <n.arranz.agueda@gmail.com>
2021-03-19 23:13:32 +0100

    security surveillance using a simple webcam

---
+++ webcam/security_webcam.py
@@ -0,0 +1,30 @@
+import cv2
+import logging
+import datetime
+
+logger = logging.getLogger(__name__)
+
+cam = cv2.VideoCapture('/dev/video2')
+
+total = 70
+current = 0
+
+ret, frame = cam.read()
+
+tic = datetime.datetime.now()
+
+while True:
+    if current >= total:
+        ret, frame = cam.read()
+        cv2.imshow('frame', frame)
+        if (datetime.datetime.now() - tic) >  datetime.timedelta(seconds=1):
+            tic = datetime.datetime.now()
+        current = 0
+    current += 1
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
+
