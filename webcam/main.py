473bb36c84a72d6b2de879565c07e2c765c49397
igarag <n.arranz.agueda@gmail.com>
2021-03-21 20:56:42 +0100

    simple flask server using camera in streaming

--- webcam/main.py
+++ webcam/main.py
@@ -1,12 +1,55 @@
-from flask import render_template
-from flask import Flask, url_for
+import datetime
+import logging
 
+import cv2
+from flask import Flask, Response, render_template, url_for
 
 app = Flask(__name__)
+logger = logging.getLogger(__name__)
+
+camera = cv2.VideoCapture(0)
+
+def get_camera_from_os():
+    devices = 4
+    for device in range(devices):
+        camera = cv2.VideoCapture(f'/dev/video{device}')
+        
+        if camera != None:
+            logging.info(f'Camera {device} selected.')
+            break
+        else:
+            logger.error('No camera found')
+    return camera
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()
+        if not success:
+            logger.error('No camera found.')
+            break
+        else:
+            logger.info('Sending frames')
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
-def hello(name=None):
-    return render_template('index.html', name=name)
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8000)
+
+    app.run(debug=True,
+            host='0.0.0.0',
+            port=8000,
+            threaded=True,
+            use_reloader=False)
+
+    camera.release()
